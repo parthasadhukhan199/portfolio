e1c7c8e38dac1cc1377384e025217c5ae93dc430
parthasadhukhan199 <parthasadhukhan199@gmail.com>
2025-09-03 11:16:25 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss(),],
-  base: '/https://github.com/parthasadhukhan199/portfolio/',
+  base: '/portfolio/',
 
 })
